2ed7ec763659c7970455173818c5430c29275525
Toni Stephens <98834731+tonistephens@users.noreply.github.com>
2025-07-20 20:04:06 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -28,31 +28,34 @@ def addition(x,y):
     return x
 
 def multiply(x,y):
+    negative = False
+    if y < 0:
+        negative = not negative
+        y = -y
+    if x < 0:
+        negative = not negative
+        x = -x
     m = 0
-    if y == 0:
-        x = 0
-    elif y > 0:
-        for i in range(y):
-            m += x
-    elif y < 0:
-        y = abs(y)
-        for i in range(y):
-            m -= x
-    return m
+    for i in range(y):
+        m += x
+    return -m if negative else m
 
 def division(x,y):
-    count = 0
     if y == 0:
-        count = "Error. Cannot divide by zero."
-    elif y > 0:
-        while x > y:
-            x -= y
-            count += 1
-    elif y < 0:
-        y = abs(y)
-        while x > y:
-            x -= y
-            count += 1
-    return count
+        return "Error. Cannot divide by zero."
+    if x == 0:
+        return 0
+    negative = False
+    if x < 0:
+        negative = not negative
+        x = -x
+    if y < 0:
+        negative = not negative
+        y = -y
+    count = 0
+    while x >= y:
+        x -= y
+        count += 1
+    return -count if negative else count
 
 main()
